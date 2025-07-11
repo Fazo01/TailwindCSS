b779200d4c0885773707b4ae248daec44f51cc38
Fazo01 <fg7829098@gmail.com>
2025-07-11 22:31:13 +0500

    Understand how to connect with css file

---
+++ Lecture-11/app.js
@@ -0,0 +1,23 @@
+const path=require('path')
+const rootDir=require('./utils/pathutils')
+const express=require('express')
+const app=express()
+const userRouter=require('./routes/useRouter')//Export Router
+const Hostrouter=require('./routes/Hostrouter')//Export Router
+app.use((req,res,next)=>{
+  console.log(req.url,req.method)
+  next()
+})
+
+app.use(express.urlencoded())//parcel
+app.use(userRouter)//Export Router
+app.use("/host",Hostrouter)//Export Router /host for overall path sharing
+app.use(express.static(path.join(rootDir,'public')))//To access css file
+
+app.use((req,res,next)=>{
+  res.status(404).sendFile(path.join(rootDir,'views','page_404.html'))//Saving PAth
+})
+const PORT=3002
+app.listen(PORT,()=>{
+  console.log(`Server is running on port http://localhost:${PORT}`)
+})
