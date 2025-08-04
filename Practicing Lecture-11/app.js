c67bfdab6742233d125c974af00cca406be40917
Fazo01 <fg7829098@gmail.com>
2025-08-04 10:36:00 +0500

    Connect nodeJS html file with css Practicing Lecture-11

--- Practicing Lecture-11/app.js
+++ Practicing Lecture-11/app.js
@@ -4,6 +4,8 @@ const app=express()
 const hostRouter=require("./Routes/hostRouter")
 const useRouter=require("./Routes/useRouter")
 const rootDir=require("./utils/utilPath")
+
+app.use(express.static(path.join(rootDir,"public")))
 app.use((req,res,next)=>{
   console.log(req.method)
   next()
@@ -11,7 +13,7 @@ app.use((req,res,next)=>{
 app.use(useRouter)
 app.use("/host",hostRouter)
 app.use((req,res,next)=>{
-  res.sendFile(path.join(rootDir,"Views","page_404.html"))
+  res.status(404).sendFile(path.join(rootDir,"Views","page_404.html"))
 })
 const PORT=3000
 app.listen(PORT,()=>{
